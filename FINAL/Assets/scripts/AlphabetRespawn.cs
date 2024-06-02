165477f4d77c47d8b19231972aad9bbbfafc0560
alex <jyhan0625@gmail.com>
2024-06-02 17:08:46 +0900

    한정연-메인화면

---
+++ FINAL/Assets/scripts/AlphabetRespawn.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphabetRespawn : MonoBehaviour
+{
+    public GameObject[] alphabetPrefabs; // A, S, D ���ĺ� �̹��� ������ �迭
+    public float spawnInterval = 1f; // ���ĺ� ���� �ֱ� (�� ����)
+    private RectTransform canvasRectTransform; // Canvas�� RectTransform
+    private float padding = 20f; // ȭ�� ��迡���� �е� ��
+
+    void Start()
+    {
+        canvasRectTransform = GetComponentInParent<Canvas>().GetComponent<RectTransform>(); // �θ� Canvas�� RectTransform ��������
+        StartCoroutine(SpawnAlphabet());
+    }
+
+    IEnumerator SpawnAlphabet()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+
+            // ���� ���ĺ� ����
+            GameObject alphabetPrefab = GetRandomAlphabetPrefab();
+
+            // ���ο� ���ĺ� �̹��� ����
+            GameObject newAlphabet = Instantiate(alphabetPrefab, transform);
+
+            // ���� ��ġ ����
+            RectTransform rectTransform = newAlphabet.GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = GetRandomPosition(rectTransform.sizeDelta);
+
+            // ����� �Է��� ��ٸ�
+            yield return WaitForInput(newAlphabet);
+
+            // ����ڰ� ���ĺ��� ������ �ʰ� �ð��� ������ ���ĺ� ����
+            Destroy(newAlphabet);
+        }
+    }
+
+    GameObject GetRandomAlphabetPrefab()
+    {
+        // A, S, D �� �����ϰ� ����
+        int randomIndex = Random.Range(0, 3);
+        return alphabetPrefabs[randomIndex];
+    }
+
+    Vector2 GetRandomPosition(Vector2 size)
+    {
+        float x = Random.Range(canvasRectTransform.rect.width - 2300, canvasRectTransform.rect.width - 1000);
+        float y = Random.Range(canvasRectTransform.rect.height - 1300, canvasRectTransform.rect.height - 600);
+        return new Vector2(x, y);
+    }
+
+    IEnumerator WaitForInput(GameObject alphabet)
+    {
+        bool keyPressed = false;
+        while (!keyPressed)
+        {
+            if (Input.GetKeyDown(KeyCode.A) && alphabet.name == "A(Clone)")
+            {
+                keyPressed = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.S) && alphabet.name == "S(Clone)")
+            {
+                keyPressed = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.D) && alphabet.name == "D(Clone)")
+            {
+                keyPressed = true;
+            }
+            yield return null;
+        }
+    }
+
+    public void Activate()
+    {
+        this.enabled = true;
+        Debug.Log("AlphabetRespawn has been activated!");
+    }
+}
---
+++ FINAL/Assets/scripts/ButtonController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    public Button triggerButton;
+    public AlphabetRespawn alphabetRespawnScript;
+    public GameObject canvasObject; // Canvas 객체 참조
+    public GameObject Play;
+    public GameObject Credit;
+    
+
+    void Start()
+    {
+        if (triggerButton != null && canvasObject != null)
+        {
+            // 버튼에 클릭 이벤트 리스너 추가
+            triggerButton.onClick.AddListener(OnButtonClicked);
+        }
+        else
+        {
+            Debug.LogError("Trigger button or Canvas object is not assigned.");
+        }
+    }
+
+    void OnButtonClicked()
+    {
+        // AlphabetRespawn 스크립트 활성화
+
+        alphabetRespawnScript.Activate();
+        Destroy(Play);
+        Destroy(Credit);
+        
+
+    }
+}
